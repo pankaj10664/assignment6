04edb4b3d71abba4fb461f43ed6d6ebdee587ce5
pankaj10664 <pankaj10664@gmail.com>
2018-06-13 15:34:23 +0530

    use of static block

---
+++ big.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Blck
+ {
+   static int n;
+   static String s;
+   static{
+      n = 3;
+      s="this is java class";
+   }
+   public static void main(String args[])
+   {
+      System.out.println("Value of number: "+n);
+      System.out.println("Value of string: "+s);
+   }
+}
